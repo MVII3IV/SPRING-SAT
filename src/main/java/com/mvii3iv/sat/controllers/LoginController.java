1df548c89187d2b441f67e63d1f3e3e5d02067ba
Dominguez <erick.dominguez@autozone.com>
2017-10-13 16:35:05 -0600

    captcha added

--- src/main/java/com/mvii3iv/sat/controllers/LoginController.java
+++ src/main/java/com/mvii3iv/sat/controllers/LoginController.java
@@ -6,18 +6,28 @@ import com.gargoylesoftware.htmlunit.DefaultCredentialsProvider;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlImage;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.LogFactory;
+import org.apache.xerces.impl.dv.util.Base64;
 import org.omg.CORBA_2_3.portable.InputStream;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 @Controller
@@ -41,7 +51,8 @@ public class LoginController {
 
 
             HtmlImage image = page.<HtmlImage>getFirstByXPath("//*[@id='IDPLogin']/div[3]/label/img");
-            File imageFile = new File("C:\\Users\\edomingu\\Desktop\\SATDemo\\src\\main\\resources\\static\\img\\captcha.jpg");
+            //File imageFile = new File("C:\\Users\\edomingu\\Desktop\\SATDemo\\src\\main\\resources\\static\\img\\captcha.jpg");
+            File imageFile = new File("C:\\captcha.jpg");
 
             image.saveAs(imageFile);
 
@@ -51,6 +62,62 @@ public class LoginController {
     }
 
 
+
+    @ResponseBody
+    @RequestMapping(value = "/image", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+    public void getLastReport(HttpServletResponse response)
+            throws IOException {
+
+        init();
+
+        HtmlPage page = null;
+        try {
+            page = webClient.getPage("https://portalcfdi.facturaelectronica.sat.gob.mx/");
+
+
+            System.out.println(page.getTitleText());
+
+
+            HtmlImage image = page.<HtmlImage>getFirstByXPath("//*[@id='IDPLogin']/div[3]/label/img");
+            File imageFile = new File("C:\\Users\\edomingu\\Desktop\\SATDemo\\src\\main\\resources\\static\\img\\captcha.jpg");
+
+
+
+            image.saveAs(imageFile);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        String captcha = "";
+        byte[] bytes = null;
+        String loginTemplate = "";
+
+        try {
+            loginTemplate = FileUtils.readFileToString(   new File(this.getClass().getResource("/templates/login.html").toURI()), "UTF-8");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+
+
+        //try (java.io.InputStream templateStream = getClass().getResourceAsStream("/img/captcha.jpg")) {
+        Path path = Paths.get("C:\\Users\\edomingu\\Desktop\\SATDemo\\src\\main\\resources\\static\\img\\captcha.jpg");
+        bytes = Files.readAllBytes(path);
+
+
+
+        String encodedImage = Base64.encode(bytes);
+        loginTemplate = loginTemplate.replace("$captcha",encodedImage);
+
+
+        response.getOutputStream().write(loginTemplate.getBytes());
+
+        //response.getOutputStream().write("The report is not available".getBytes());
+
+    }
 
 
     private void login() {

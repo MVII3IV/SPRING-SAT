38b0309d4b369b3ef2e753fc9666dcd6552b3003
Erick Mullen <afrosclothing@hotmail.com>
2018-01-08 16:25:42 -0700

    morris charts added

--- src/main/resources/static/angular-app/mainController.js
+++ src/main/resources/static/angular-app/mainController.js
@@ -1,10 +1,68 @@
-angular.module('app').controller("mainController", ['$scope', function($scope){
-    $scope.menuElements = [ {section: "Ingresos", class: "Active"},
-    {
-        section:"Gastos",
-        class: "Active"
-    }, {
-    section: "Simulador de Impuestos",
-    class: "Active"
-    }, {section: "Declaracion", class: "Active"}, {section: "Facturas", class: "Active"}, {section: "Nominas", class: "Active"}, {section:"IMMS", class: "Active"}, {section: "Pagos",class: "Active"}, {section:"Ayuda", class: "Active"}];
+angular.module('app').controller("mainController", ['$scope', function ($scope) {
+
+    $scope.menuElements = [{
+            section: "Pagina Principal",
+            class: "active"
+        },
+        {
+            section: "Ingresos",
+            class: ""
+        },
+        {
+            section: "Gastos",
+            class: ""
+        }, {
+            section: "Simulador de Impuestos",
+            class: ""
+        }, {
+            section: "Declaracion",
+            class: ""
+        }, {
+            section: "Facturas",
+            class: ""
+        }, {
+            section: "Nominas",
+            class: ""
+        }, {
+            section: "IMMS",
+            class: ""
+        }, {
+            section: "Pagos",
+            class: ""
+        }, {
+            section: "Ayuda",
+            class: ""
+        }
+    ];
+
+
+
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+      $scope.xaxis = 'y';
+      $scope.yaxis = '["a,b"]';
+
+     	$scope.data = [
+        { y: "2006-07", a: 100, b: 90 },
+        { y: "2007-06", a: 75,  b: 65 },
+        { y: "2008-05", a: 50,  b: 40 },
+        { y: "2009-04", a: 40,  b: 65 },
+        { y: "2010-03", a: 30,  b: 40 },
+        { y: "2011-02", a: 10,  b: 65 },
+        { y: "2012-01", a: 0, b: 90 }
+    	];
+
+      $scope.xLabelFormat = function(x) { // <--- x.getMonth() returns valid index
+        var month = months[x.getMonth()];
+        return month;
+      }
+
+      $scope.dateFormat = function(x) {
+        var month = months[new Date(x).getMonth()];
+        return month;
+      }
+
+
+
+
 }]);

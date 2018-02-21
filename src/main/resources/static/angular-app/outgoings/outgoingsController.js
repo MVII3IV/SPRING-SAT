044caeb514df5ccc3630594e7a8772c204f82d08
Erick Mullen <afrosclothing@hotmail.com>
2018-02-21 16:30:25 -0700

    new views added

--- src/main/resources/static/angular-app/outgoings/outgoingsController.js
+++ src/main/resources/static/angular-app/outgoings/outgoingsController.js
@@ -1,3 +1,48 @@
-angular.module('app').controller("outgoingsController", ['$scope', function ($scope) {
-    $scope.title = "Gastos";
+angular.module('app')
+
+.directive('loadFinished', function() {
+      return function(scope, element, attrs) {
+        if (scope.$last){
+            $('body').css('height', $('.dev-page-content').prop('scrollHeight') + 20);
+        }
+      };
+    })
+
+.controller("outgoingsController", ['$scope', '$http', function ($scope, $http) {
+        $scope.title = "Gastos";
+        $scope.grandTotal = 0;
+
+        $scope.bills = [
+            {
+                    "fiscalId": "Folio Fiscal",
+                    "emisorRFC": "RFC Emisor",
+                    "emisorName": "Nombre o Razón Social del Emisor",
+                    "receiverRFC": "RFC Receptor",
+                    "receiverName": "Nombre o Razón Social del Receptor",
+                    "emitedDate": "Fecha de Emisión",
+                    "certificationDate": "Fecha de Certificación",
+                    "certifiedPAC": "PAC que Certificó",
+                    "total": "Total",
+                    "voucherEffect": "Efecto del Comprobante",
+                    "voucherStatus": "Estado del Comprobante"
+                }
+        ];
+
+
+        $http.get("../incomes/").then(function mySuccess(response) {
+            $scope.bills = response.data;
+
+        }, function myError(response) {
+            $scope.myWelcome = response.statusText;
+        });
+
+
+        $scope.getTotal = function(value){
+            var total = 0;
+            value.forEach(function(iteratedElement){
+                total += Number(iteratedElement.total.replace(/[^0-9\.-]+/g,""))
+            });
+            $scope.grandTotal += total;
+            return total;
+        }
 }]);
--- src/main/resources/static/angular-app/tax-simulator/taxSimulatorController.js
+++ src/main/resources/static/angular-app/tax-simulator/taxSimulatorController.js
@@ -1,3 +1,71 @@
 angular.module('app').controller("taxSimulatorController", ['$scope', function ($scope) {
     $scope.title = "Simulador de Impuestos";
+
+    $scope.taxes = [
+         {
+            lowerLimit : 0.01,
+            maxLimit: 285.45,
+            fixedFee: 0.00,
+            aboveInferiorLimit: 1.92
+         },
+         {
+            lowerLimit : 285.46,
+            maxLimit: 2422.80,
+            fixedFee: 5.55,
+            aboveInferiorLimit: 6.40
+         },
+         {
+             lowerLimit : 2422.81,
+             maxLimit: 4257.90,
+             fixedFee: 142.20,
+             aboveInferiorLimit: 10.88
+         },
+         {
+             lowerLimit : 4257.91,
+             maxLimit: 4949.55,
+             fixedFee: 341.85,
+             aboveInferiorLimit: 16.00
+         },
+         {
+              lowerLimit : 4949.56,
+              maxLimit: 5925.90,
+              fixedFee: 452.55,
+              aboveInferiorLimit: 17.92
+          },
+          {
+               lowerLimit : 5925.91	,
+               maxLimit: 11951.85,
+               fixedFee: 627.60,
+               aboveInferiorLimit: 21.36
+           }, {
+               lowerLimit : 18837.76,
+               maxLimit: 35964.30,
+               fixedFee: 3534.30,
+               aboveInferiorLimit: 30.00
+           }, {
+               lowerLimit : 35964.31,
+               maxLimit: 47952.30,
+               fixedFee: 8672.25,
+               aboveInferiorLimit: 32.00
+           }, {
+               lowerLimit : 47952.31,
+               maxLimit: 143856.90,
+               fixedFee: 12508.35,
+               aboveInferiorLimit: 34.00
+           }, {
+               lowerLimit : 143856.91,
+               maxLimit: 'En adelante',
+               fixedFee: 45115.95,
+               aboveInferiorLimit: 35.00
+           }
+
+
+    ];
 }]);
+
+
+
+
+
+
+

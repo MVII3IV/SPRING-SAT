0a50d664d82bdefeff1a223bad25e44e59560432
Erick Mullen <afrosclothing@hotmail.com>
2018-03-01 13:35:26 -0700

    declarations added

--- src/main/resources/static/angular-app/tax-simulator/taxSimulatorController.js
+++ src/main/resources/static/angular-app/tax-simulator/taxSimulatorController.js
@@ -4,63 +4,97 @@ angular.module('app').controller("taxSimulatorController", ['$scope', function (
     $scope.taxes = [
          {
             lowerLimit : 0.01,
-            maxLimit: 285.45,
-            fixedFee: 0.00,
+            maxLimit: 578.52,
+            fixedFee: 0,
             aboveInferiorLimit: 1.92
          },
          {
-            lowerLimit : 285.46,
-            maxLimit: 2422.80,
-            fixedFee: 5.55,
-            aboveInferiorLimit: 6.40
+            lowerLimit : 578.53,
+            maxLimit: 4910.18,
+            fixedFee: 11.11,
+            aboveInferiorLimit: 6.4
          },
          {
-             lowerLimit : 2422.81,
-             maxLimit: 4257.90,
-             fixedFee: 142.20,
+             lowerLimit : 4910.19,
+             maxLimit: 8629.20,
+             fixedFee: 288.33,
              aboveInferiorLimit: 10.88
          },
          {
-             lowerLimit : 4257.91,
-             maxLimit: 4949.55,
-             fixedFee: 341.85,
-             aboveInferiorLimit: 16.00
+             lowerLimit : 8629.21,
+             maxLimit: 10031.07,
+             fixedFee: 692.96,
+             aboveInferiorLimit: 16
          },
          {
-              lowerLimit : 4949.56,
-              maxLimit: 5925.90,
-              fixedFee: 452.55,
+              lowerLimit : 10031.08,
+              maxLimit: 12009.94,
+              fixedFee: 917.26,
               aboveInferiorLimit: 17.92
           },
           {
-               lowerLimit : 5925.91	,
-               maxLimit: 11951.85,
-               fixedFee: 627.60,
+               lowerLimit : 12009.95,
+               maxLimit: 24222.31,
+               fixedFee: 1271.84,
                aboveInferiorLimit: 21.36
            }, {
-               lowerLimit : 18837.76,
-               maxLimit: 35964.30,
-               fixedFee: 3534.30,
-               aboveInferiorLimit: 30.00
+               lowerLimit : 24222.32,
+               maxLimit: 38177.69,
+               fixedFee: 3880.44,
+               aboveInferiorLimit: 23.52
            }, {
-               lowerLimit : 35964.31,
-               maxLimit: 47952.30,
-               fixedFee: 8672.25,
-               aboveInferiorLimit: 32.00
+               lowerLimit : 38177.70,
+               maxLimit: 72887.50,
+               fixedFee: 7162.74,
+               aboveInferiorLimit: 30
            }, {
-               lowerLimit : 47952.31,
-               maxLimit: 143856.90,
-               fixedFee: 12508.35,
-               aboveInferiorLimit: 34.00
+               lowerLimit : 72887.51,
+               maxLimit: 97183.33,
+               fixedFee: 17575.69,
+               aboveInferiorLimit: 32
            }, {
-               lowerLimit : 143856.91,
-               maxLimit: 'En adelante',
-               fixedFee: 45115.95,
-               aboveInferiorLimit: 35.00
-           }
-
-
+               lowerLimit : 97183.34,
+               maxLimit: 291550,
+               fixedFee: 25350.35,
+               aboveInferiorLimit: 34
+           },
+           , {
+              lowerLimit : 291550.01,
+              maxLimit: 0,
+              fixedFee: 91435.02,
+              aboveInferiorLimit: 35
+          }
     ];
+
+    $scope.feesToPay = 0;
+    $scope.outcomes = 0;
+    $scope.incomes = 0;
+    $scope.incomesBeforeFees = 0;
+    $scope.fees = 0;
+
+    $scope.calculteFees = function(){
+
+        $scope.incomesBeforeFees = $scope.incomes - $scope.outcomes;
+
+        if($scope.incomesBeforeFees > 0){
+            $scope.taxes.forEach(function(fee){
+                if($scope.incomes >= fee.lowerLimit && $scope.incomes <= fee.maxLimit){
+                    $scope.fees = (($scope.incomesBeforeFees - fee.lowerLimit) * (fee.aboveInferiorLimit / 100)) + fee.fixedFee;
+                }
+            });
+        }else{
+            $scope.fees = 0;
+        }
+
+
+
+
+
+
+
+    }
+
+
 }]);
 
 

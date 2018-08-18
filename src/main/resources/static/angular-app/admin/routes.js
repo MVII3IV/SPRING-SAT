3207ea87ac56e47b2c6eacc736041af5eb2bbe5b
MVII3IV <afrosclothing@hotmail.com>
2018-08-18 10:52:47 -0600

    working version

--- src/main/resources/static/angular-app/admin/routes.js
+++ src/main/resources/static/angular-app/admin/routes.js
@@ -34,10 +34,56 @@ angular.module("adminApp")
                 controller: 'customersController'
             })
 
+
+
+
+            .when('/landing-page', {
+                templateUrl: '../angular-app/customers/home/home.html',
+                controller: 'mainController'
+            })
+            .when('/income', {
+                templateUrl: '../angular-app/customers/income/income.html',
+                controller: 'incomeController'
+            })
+            .when('/outgoings', {
+                templateUrl: '../angular-app/customers/outgoings/outgoings.html',
+                controller: 'outgoingsController'
+            })
             .when('/tax-simulator', {
                 templateUrl: '../angular-app/customers/tax-simulator/tax-simulator.html',
                 controller: 'taxSimulatorController'
             })
+            .when('/procedures', {
+                templateUrl: '../angular-app/customers/procedures/procedures.html',
+                controller: 'proceduresController'
+            })
+            .when('/declarations', {
+                templateUrl: '../angular-app/customers/declarations/declarations.html',
+                controller: 'declarationsController'
+            })
+            .when('/bills', {
+                templateUrl: '../angular-app/customers/bills/bills.html',
+                controller: 'billsController'
+            })
+            .when('/paysheets', {
+                templateUrl: '../angular-app/customers/paysheets/paysheets.html',
+                controller: 'paysheetsController'
+            })
+            .when('/social-security', {
+                templateUrl: '../angular-app/customers/social-security/socialsecurity.html',
+                controller: 'socialSecurityController'
+            })
+            .when('/payments', {
+                templateUrl: '../angular-app/customers/payments/payments.html',
+                controller: 'paymentsController'
+            })
+            .when('/help', {
+                templateUrl: '../angular-app/customers/help/help.html',
+                controller: 'helpController'
+            })
+
+
+
 
             .otherwise('/');
     }]);

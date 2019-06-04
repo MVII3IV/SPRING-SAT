aea580a45e2a1d2b2ad93fad878b23226d31f595
Erick Dominguez <erick.dominguez@autozone.com>
2019-06-04 10:43:29 -0600

    order logic simplified thanks to Miguel

--- src/main/resources/static/angular-app/customers/bills/billsService.js
+++ src/main/resources/static/angular-app/customers/bills/billsService.js
@@ -4,7 +4,6 @@ app.service('billsService', ['$http', 'userService', function($http, userService
     var years = [2018, 2019];
     var months = [01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12];
 
-
     var billsData =
     {
         "emitted" : [
@@ -24,59 +23,17 @@ app.service('billsService', ['$http', 'userService', function($http, userService
     }
 
     this.orderData = function(bills){
-        bills = sortYear(bills);
-        bills = sortMonth(bills);
-        bills = sortDay(bills);
+
+        var orderedByName = bills.sort(function (a, b) {
+            var dateAParts = a.emitedDate.split("/");
+            var dateBParts = b.emitedDate.split("/");
+            return  (new Date(+dateAParts[2], dateAParts[1] - 1, +dateAParts[0]) < new Date(+dateBParts[2], dateBParts[1] - 1, +dateBParts[0])) ? 1 : ((new Date(+dateBParts[2], dateBParts[1] - 1, +dateBParts[0]) < new Date(+dateAParts[2], dateAParts[1] - 1, +dateAParts[0]))? -1 : 0)
+        });
+
+
     }
 
 
-    function sortYear(arr) {
-        var len = arr.length;
-
-        for (var i = 0; i < len ; i++) {
-            for(var j = 0 ; j < len - i - 1; j++){ // this was missing
-                if (  (parseInt(arr[j].emitedDate.split('/')[2]) > parseInt(arr[j + 1].emitedDate.split('/')[2])) ) {
-                    // swap
-                    var temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-        return arr;
-    }
-
-    function sortMonth(arr) {
-        var len = arr.length;
-
-        for (var i = 0; i < len ; i++) {
-            for(var j = 0 ; j < len - i - 1; j++){ // this was missing
-                if (  (parseInt(arr[j].emitedDate.split('/')[1]) > parseInt(arr[j + 1].emitedDate.split('/')[1])) &&  (parseInt(arr[j].emitedDate.split('/')[2]) == parseInt(arr[j + 1].emitedDate.split('/')[2]))  ) {
-                    // swap
-                    var temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-        return arr;
-    }
-
-    function sortDay(arr) {
-        var len = arr.length;
-
-        for (var i = 0; i < len ; i++) {
-            for(var j = 0 ; j < len - i - 1; j++){ // this was missing
-                if (  (parseInt(arr[j].emitedDate.split('/')[0]) > parseInt(arr[j + 1].emitedDate.split('/')[0])) &&  (parseInt(arr[j].emitedDate.split('/')[1]) == parseInt(arr[j + 1].emitedDate.split('/')[1])) ) {
-                    // swap
-                    var temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-        return arr;
-    }
 
 
 }]);
